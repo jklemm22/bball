892a7ce45bcb009db8d3618a05ffcd4e7f792aa8
Jeremy K <jklemm22@gmail.com>
2015-09-13 14:50:46 -0400

    Adds a dummy teams file

---
+++ teams.js
@@ -0,0 +1,68 @@
+[
+  {
+    "_id": "55f5c49c32fa2ce2b47b47bd",
+    "name": "Team eiusmod",
+    "managers": [
+      {
+        "name": "Hannah Schultz",
+        "id": 5
+      },
+      {
+        "name": "Elnora Velasquez",
+        "id": 11
+      }
+    ]
+  },
+  {
+    "_id": "55f5c49cb0b3fe469f434934",
+    "name": "Team qui",
+    "managers": [
+      {
+        "name": "Hunt Russell",
+        "id": 6
+      },
+      {
+        "name": "Juliette Ellison",
+        "id": 16
+      }
+    ]
+  },
+  {
+    "_id": "55f5c49c2ca0d018fed8d490",
+    "name": "Team dolore",
+    "managers": [
+      {
+        "name": "Carney Lopez",
+        "id": 10
+      }
+    ]
+  },
+  {
+    "_id": "55f5c49cc4e4ad5daa11ac68",
+    "name": "Team nisi",
+    "managers": [
+      {
+        "name": "Lancaster Horne",
+        "id": 18
+      },
+      {
+        "name": "Randall Price",
+        "id": 10
+      }
+    ]
+  },
+  {
+    "_id": "55f5c49cc887c2df181f783f",
+    "name": "Team et",
+    "managers": [
+      {
+        "name": "Colette Neal",
+        "id": 20
+      },
+      {
+        "name": "Bridget Foreman",
+        "id": 18
+      }
+    ]
+  }
+]
